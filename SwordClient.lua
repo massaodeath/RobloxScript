a02570684cb4a8c1a068eb314410178b942281f6
Fernando Massao <138241756+massaodeath@users.noreply.github.com>
2026-09-23 14:03:03 -0300

    Delete SwordClient.lua

--- SwordClient.lua
+++
@@ -1,8 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local tool = script.Parent
-local attackRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("AttackRemote")
-
-tool.Activated:Connect(function()
-	attackRemote:FireServer()
-end)
