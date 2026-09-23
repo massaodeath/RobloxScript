3e83c0e7908e8248bc56d48746f8c3e660283bcc
Fernando Massao <138241756+massaodeath@users.noreply.github.com>
2026-09-22 21:56:54 -0300

    SwordClient.lua

--- SwordClient.lua
+++ SwordClient.lua
@@ -4,6 +4,5 @@ local tool = script.Parent
 local attackRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("AttackRemote")
 
 tool.Activated:Connect(function()
-	-- Não mandamos alvo, dano, XP nem dinheiro. O servidor encontra e valida o alvo.
 	attackRemote:FireServer()
 end)
