99efaef99caf621a09e41c435e34d4416d1a01d5
Fernando Massao <138241756+massaodeath@users.noreply.github.com>
2026-09-23 13:49:46 -0300

    atualizacao

--- QuestConfig.lua
+++ QuestConfig.lua
@@ -1,25 +1,32 @@
-﻿local QuestConfig = {}
+local QuestConfig = {}
 
 QuestConfig.QuestName = "Caçador de Bandidos"
+
 QuestConfig.RequiredKills = 3
+
 QuestConfig.RewardXP = 100
 QuestConfig.RewardGold = 50
+
 QuestConfig.Enemies = {
+
 	Bandit1 = {
 		DisplayName = "Bandido da Ponte",
 		RespawnTime = 15,
 		DropChance = 35,
 	},
+
 	Bandit2 = {
 		DisplayName = "Bandido da Floresta",
 		RespawnTime = 15,
 		DropChance = 35,
 	},
+
 	Bandit3 = {
 		DisplayName = "Chefe Bandido",
 		RespawnTime = 15,
 		DropChance = 100,
 	},
+
 }
 
 QuestConfig.AttackDamage = 25
